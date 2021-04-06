a7eff700d3a63c87ed238671defd96e3dc1bbd70
Jonathan Chow <jonathan@cychow.co.uk>
2021-04-06 14:12:21 +0100

    Implemented start_clock method

--- clock.py
+++ clock.py
@@ -3,29 +3,24 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Clock():
-    def __init__(self):
+    def __init__(self, url):
         self.chrome_location = '/usr/bin/google-chrome'
+        self.clock_url = url
 
     def start_clock(self):
-        driver = webdriver.Chrome(self.chrome_location)
-        driver.get("http://www.python.org")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options)
+        # driver.get('http://hurry-app.appspot.com/12:34/Final')
+        driver.get(self.clock_url)
+        driver.fullscreen_window()
 
-# clock = Clock()
-# clock.start_clock()
+if __name__ == '__main__':
+    clock = Clock('http://hurry-app.appspot.com/12:34/Final')
+    clock.start_clock()
 
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-# caps = options.to_capabilities()
-# driver = webdriver.Chrome(ChromeDriverManager().install(),desired_capabilities=caps)
-driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options)
-
-
-driver.get('http://hurry-app.appspot.com/12:34/Final')
-driver.fullscreen_window()
 
 # see this link 
 # https://stackoverflow.com/questions/29858752/error-message-chromedriver-executable-needs-to-be-available-in-the-path
-
-
-a= 1 
