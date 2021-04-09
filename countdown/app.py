7b8162691ebf6342888d0ed4dd6ce6ae628c948b
Jonathan Chow <jonathan@cychow.co.uk>
2021-04-09 12:22:55 +0100

    WIP

--- countdown/app.py
+++ countdown/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
 from countdown.flask_config import Config
 import logging
-from countdown.clock import Clock
+# from countdown.clock import Clock
 from countdown.utils import get_clock_path
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+options = Options()
+chromium_path = '/usr/bin/chromium-browser'
+options.binary_location = chromium_path
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(chrome_options=options)
+
 
 @app.route('/')
 def index():
@@ -23,12 +33,15 @@ def start_countdown():
 
     base_url = get_clock_path()
 
-    clock = Clock(base_url, start_time, '')
+    # clock = Clock(base_url, start_time, '')
 
     logging.info(f"Start Counting Down")
     
     # clock.start_clock()
-    clock.start_clock_chromium()
+    # clock.start_clock_chromium()
+    driver.get('http://www.google.com')
+    driver.fullscreen_window()
+
 
     return redirect(url_for('index'))
 
@@ -40,10 +53,12 @@ def send_message():
     
     base_url = get_clock_path()
 
-    clock = Clock(base_url, '', message)
+    # clock = Clock(base_url, '', message)
 
-    clock.start_clock_chromium()
-    
+    # clock.start_clock_chromium()
+    driver.get('file:///home/jonathan/Documents/Repo/countdown/index.html?message=hello')
+    driver.fullscreen_window()
+
     return redirect(url_for('index'))
 
 
